1585b45ee18921e80446c1efb8e87aaad7708fc9
kibermaks <kibermaks@gmail.com>
2021-09-15 13:29:08 +0300

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,48 @@
-window.function = function (value) {
-  if (value.value === undefined) return undefined;
-  return new Date((new Date(value.value)).getTime() + 24 * 60 * 60 * 1000);
+window.function = function (value, postfix, prefix, showEmpty, showThousands, fixedNumbers, roundMode, noSpaces, emptySubstitution) {
+
+
+  showEmpty = showEmpty.value ?? false
+  value = value.value
+
+  if (!showEmpty && (value == null || value == 0))
+  {
+    emptySubstitution = emptySubstitution.value ?? ""
+    return emptySubstitution
+  } 
+
+  showThousands = showThousands.value ?? false
+
+  const formatNumber = (v) => {
+    if (roundMode.value != null)
+    {
+      if (roundMode.value < 0){
+        v = Math.floor(v)
+      }
+      else if (roundMode.value > 0){
+        v = Math.ceil(v)
+      }
+      else
+      {
+        v = Math.round(v)
+      }
+    }
+    if (fixedNumbers.value != null) {
+      v = v.toFixed(fixedNumbers.value)
+    }
+    v = String(v)
+    v = v.replace('.', ',')
+    if (showThousands) {
+      v = v.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ')
+    }
+    return v
+  }
+  noSpaces = noSpaces.value ?? false
+  let result = formatNumber(value)
+  if (postfix.value) {
+    result = result + (noSpaces ? "" : " ") + postfix.value
+  }
+  if (prefix.value) {
+    result = prefix.value + (noSpaces ? "" : " ") + result
+  }
+  return result
 }
